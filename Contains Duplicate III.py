9618951da6d5c7276a48412044ef5c20ebc6b903
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-03 02:20:18 +1000

    Add files via upload

---
+++ Contains Duplicate III.py
@@ -0,0 +1,20 @@
+#Contains Duplicate III
+
+
+        if k < 1 or t < 0:
+            return False
+
+        dic = collections.OrderedDict()
+        for n in nums:
+
+            key = n if not t else n // t
+            for m in (dic.get(key - 1), dic.get(key), dic.get(key + 1)):
+
+                if m is not None and abs(n - m) <= t:
+                    return True
+            if len(dic) == k:
+
+                dic.popitem(False)
+            
+            dic[key] = n
+        return False
