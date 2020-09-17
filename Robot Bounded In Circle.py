32e2933ff46576f6153406a5238ddebc87c25ac3
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-17 16:53:29 +0800

    Add files via upload

---
+++ Robot Bounded In Circle.py
@@ -0,0 +1,20 @@
+#Robot Bounded In Circle
+
+class Solution(object):
+    def isRobotBounded(self, instructions):
+        """
+        :type instructions: str
+        :rtype: bool
+        """
+        dirs = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+        x, y = 0, 0
+        curd = 0
+        for i in instructions:
+            if i == "G":
+                x += dirs[curd][0]
+                y += dirs[curd % 4][1]
+            elif i == "L":
+                curd = (curd + 1) % 4
+            elif i == "R":
+                curd = (curd - 1) % 4
+        return (x == 0 and y == 0) or curd != 0
